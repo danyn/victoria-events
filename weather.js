d3b9dd9a571e5dbb9d0f95168b473788869a82c2
Daniel Eisen <eisen.dan@gmail.com>
2018-02-11 22:26:24 -0800

    Create weather.js

---
+++ weather.js
@@ -0,0 +1,27 @@
+"use strict";
+
+(function(){
+  var url = "api.openweathermap.org/data/2.5/weather?id=6174032";
+  var apiKey = "63c24fb9208c9c02b3ee359d75e52dbf";
+  var httpRequest;
+  makeRequest(); 
+  
+  // create and send an XHR request 
+  function makeRequest(){
+    httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = responseMethod;
+    httpRequest.open('GET', `${url}&appid=${apiKey}`);
+    httpRequest.send();
+    
+   }//fn makeRequest
+   
+   //Handle XHR response 
+   function responseMethod(){
+    if(httpRequest.readyState === 4){
+      console.log(httpRequest.responseText);
+    }
+   }//fn responseMethod
+    
+  
+
+})();
