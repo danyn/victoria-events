1bf5340522061f27afef20f3d8ea60c63fd3596f
Daniel Eisen <eisen.dan@gmail.com>
2018-02-11 23:01:30 -0800

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,6 +1,7 @@
 "use strict";
 
 (function(){
+  var weatherBox = document.getElementById("weather");
   var url = "https://api.openweathermap.org/data/2.5/weather?id=6174032";
   var apiKey = "63c24fb9208c9c02b3ee359d75e52dbf";
   var httpRequest;
@@ -18,10 +19,26 @@
    //Handle XHR response 
    function responseMethod(){
     if(httpRequest.readyState === 4){
+      if(httpRequest.status === 200){
+        updateUISuccess(htppRequest.responseText);
+      }else{
+        updateUIError();
+      }
       console.log(httpRequest.responseText);
     }
    }//fn responseMethod
     
+  // handle xhr sucess
+  function updateUISuccess(responseText) {
+    var response = JSON.parse(responseText);
+    var condition = response.weather[0];
+    var temp = response.main.temp;
+    weatherBox.innerHTML = `<p>It is currently ${temp} &#176 degrees.</p><p>${condition}</p>` ;
+  }
+// handle xhr error
+  function updateUIError() {
+    weatherBox.className = "hidden";
+    
+  }
   
-
 })();
